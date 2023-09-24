3c4be1c8893f550ed2e6266d95f4b589883a557a
Ali Modaresi <modaresi.mr@gmail.com>
2023-09-24 19:37:20 +0200

    upodate

--- scripts/node_multi_run.sh
+++ scripts/node_multi_run.sh
@@ -2,28 +2,31 @@
 
 mkdir -p logs
 
-dataset=0 1 2 3 4
-dataset=0
-seg=0 1 2
-seg=0
-models=0 1 2 3 4 5
-feat=0
-for d in $dataset
-for s in $seg;do
-for f in $feat;do
-for m in $models;do
+# Declaring arrays for datasets, segments, models, and features
+datasets=(0 1 2 3 4)
+datasets=(0)
+segs=(0 1 2)
+segs=(0)
+models=(0 1 2 3 4 5)
+feats=(0)
 
-# for siz in 1 2 5 15`seq 10 10 60`;do
-#   for ove in 1 2 5 15 `seq 10 10 $s`;do
-for siz in 30;do
-  for ove in 5;do
-  	sp="size=$siz,shift=$ove"
+# Looping through each array
+for d in "${datasets[@]}"; do
+  for s in "${segs[@]}"; do
+    for f in "${feats[@]}"; do
+      for m in "${models[@]}"; do
+      # for siz in 1 2 5 15`seq 10 10 60`;do
+        #   for ove in 1 2 5 15 `seq 10 10 $s`;do
+        for siz in 30; do
+          for ove in 5; do
+            sp="size=$siz,shift=$ove"
 
-    srun -N 1-1 --gpus=1 cat /etc/hostname
-    ./node_single_run.sh -c comment -d $d -s $s -sp $sp -f $f  -st 0 -m $m
-
-done
-done
-done
-done
+            # Running the commands
+            srun -N 1-1 --gpus=1 cat /etc/hostname
+            ./node_single_run.sh -c comment -d "$d" -s "$s" -sp "$sp" -f "$f"  -st 0 -m "$m"
+          done
+        done
+      done
+    done
+  done
 done
